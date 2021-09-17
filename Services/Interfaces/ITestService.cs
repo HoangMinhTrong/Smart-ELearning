a00aca772fe993b5f957d23d60d1f8a981313c85
letuan32 <letuanlttt@gmail.com>
2021-09-17 11:56:19 +0700

    CRUD-subject
    CRUD-subject

--- Services/Interfaces/ITestService.cs
+++ Services/Interfaces/ITestService.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Smart_ELearning.Services.Interfaces
 {
-    public interface ITestService
+    internal interface ITestService
     {
     }
 }
--- Services/TestService.cs
+++ Services/TestService.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Smart_ELearning.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Smart_ELearning.Services
 {
-    public class TestService
+    public class TestService : ITestService
     {
     }
 }
