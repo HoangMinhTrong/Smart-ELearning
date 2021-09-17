1a9c6540128451467196e00f006baebc2328a5e8
HoangMinhTrong <tronggaw2@gmail.com>
2021-09-17 16:18:38 +0700

    ViewsModels

---
+++ ViewModels/ScheduleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Smart_ELearning.Models;
+
+
+namespace Smart_ELearning.ViewModels
+{
+    public class ScheduleViewModel
+    {
+        public ScheduleModel ScheduleModel{ get; set; }
+        public IEnumerable<SelectListItem> ClassListItems { get; set; }
+        public IEnumerable<SelectListItem> SubjectListItems { get; set; }
+    }
+}
