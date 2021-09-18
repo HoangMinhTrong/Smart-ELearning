f60ebe13b782b6c53e82af7693606c6ca3ef8e09
HoangMinhTrong <tronggaw2@gmail.com>
2021-09-18 20:21:38 +0700

    UpdateDatabase

--- Models/SubmitModel.cs
+++ Models/SubmitModel.cs
@@ -6,10 +6,8 @@ namespace Smart_ELearning.Models
     {
         public int Id { get; set; }
         public int ScheduleId { get; set; }
-        public string UserId { get; set; }
         public int TestId { get; set; }
         public int NumberOfCorrectAnswer { get; set; }
-        public string ListAnswer { get; set; }
         public double TotalGrade { get; set; }
 
         public StudentAttendanceModel StudentAttendanceModel { get; set; }
